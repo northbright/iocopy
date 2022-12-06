9754059f93efe808501c616e92714705e1716365
Frank Xu <frank.xu@northbright.com>
2022-12-06 11:37:45 +0800

    Add comments for iocopy

--- iocopy.go
+++ iocopy.go
@@ -15,11 +15,19 @@ const (
 	DefaultInterval = 500 * time.Millisecond
 )
 
+// Event is the interface that wraps String method.
+// When Start is called, it'll return a channel for the caller to receive
+// IO copy related events.
+// Currently, there're 3 types of events:
+// (1). EventWritten - n bytes have been written successfully.
+// (2). EventError - an error occurs and the goroutine exits.
+// (3). EventOK - IO copy succeeded.
 type Event interface {
 	// stringer
 	String() string
 }
 
+// EventWritten is the event that n bytes have been written successfully.
 type EventWritten struct {
 	written int64
 }
@@ -28,14 +36,19 @@ func newEventWritten(written int64) *EventWritten {
 	return &EventWritten{written: written}
 }
 
+// String implements the stringer interface.
 func (e *EventWritten) String() string {
 	return fmt.Sprintf("%d bytes written", e.written)
 }
 
+// Written returns the count of bytes written successfuly.
 func (e *EventWritten) Written() int64 {
 	return e.written
 }
 
+// EventError is the event that an error occurs.
+// The error can be a context error(ctx.Err()) when the context is canceled or
+// an IO related error during copy.
 type EventError struct {
 	err error
 }
@@ -44,14 +57,17 @@ func newEventError(err error) *EventError {
 	return &EventError{err: err}
 }
 
+// String implements the stringer interface.
 func (e *EventError) String() string {
 	return e.err.Error()
 }
 
+// Err returns the error occured during IO copy.
 func (e *EventError) Err() error {
 	return e.err
 }
 
+// EventOK is the event that IO copy succeeded.
 type EventOK struct {
 	written int64
 }
@@ -60,14 +76,28 @@ func newEventOK(written int64) *EventOK {
 	return &EventOK{written: written}
 }
 
+// String implements the stringer interface.
 func (e *EventOK) String() string {
 	return fmt.Sprintf("written OK(total: %d bytes)", e.written)
 }
 
+// Written returns the count of bytes written successfuly.
 func (e *EventOK) Written() int64 {
 	return e.written
 }
 
+// Start returns a channel for the caller to receive IO copy events and start a goroutine to do IO copy.
+// ctx: context.Context. It can be created using WithCancel, WithDeadline, WithTimeout...
+// dst: io.Writer to copy to.
+// src: io.Reader to copy from.
+// bufSize: size of the buffer. It'll create a buffer in the new goroutine according to the buffer size.
+// interval: interval to send EventWritten event to the channel.
+// You may set it to DefaultInterval.
+// ch: the returned channel to receive IO copy events.
+// The channel will be closed when:
+// (1). an error occured(context error or IO copy error).
+// (2). IO copy succeeded.
+// You may use a for-range loop to read events from the channel.
 func Start(
 	ctx context.Context,
 	dst io.Writer,
@@ -111,6 +141,7 @@ func Start(
 			case <-ctx.Done():
 				ch <- newEventError(ctx.Err())
 				return
+
 			default:
 				n, err := src.Read(buf)
 				if err != nil && err != io.EOF {
