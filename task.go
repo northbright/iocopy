135beeadab70f2e3f9ff9f3be86b6713d3c349ba
Frank Xu <frank.xu@northbright.com>
2024-08-13 13:49:07 +0800

    Add comments and remove useless comments

--- task.go
+++ task.go
@@ -3,16 +3,22 @@ package iocopy
 import (
 	"context"
 	"io"
+	"time"
 )
 
+// OnWritten is the type of function called by [Do] when n bytes is written(copied) successfully.
 type OnWritten func(isTotalKnown bool, total, copied, written uint64, percent float32)
 
+// OnStop is the type of function called by [Do] when copy is stopped. The cause parameter is returned by context.Err().
 type OnStop func(isTotalKnown bool, total, copied, written uint64, percent float32, cause error)
 
+// OnOK is the type of function called by [Do] when copy is done.
 type OnOK func(isTotalKnown bool, total, copied, written uint64, percent float32)
 
+// OnError is the type of function called by [Do] when error occurs.
 type OnError func(err error)
 
+// Task is the interface of io copy task which is passed to [Do].
 type Task interface {
 	Writer() io.Writer
 	Reader() io.Reader
@@ -21,10 +27,19 @@ type Task interface {
 	SetCopied(uint64)
 }
 
+// Do does io copy task and block caller's go routine until an error occurs or copy stopped by user or copy is done.
+// Parameters:
+// ctx: context.Context.
+// It can be created using context.WithCancel, context.WithDeadline,
+// context.WithTimeout...
+// t: [Task]
+// bufSize: size of the buffer. It'll create a buffer in the new goroutine according to the buffer size.
+// interval: Interval to reports n bytes written(copied) during the IO copy.
 func Do(
 	ctx context.Context,
 	t Task,
 	bufSize uint,
+	interval time.Duration,
 	onWritten OnWritten,
 	onStop OnStop,
 	onOK OnOK,
@@ -49,7 +64,7 @@ func Do(
 		w,
 		r,
 		bufSize,
-		DefaultInterval,
+		interval,
 		isTotalKnown,
 		total,
 		copied,
