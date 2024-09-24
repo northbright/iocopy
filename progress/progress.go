88dd11cee33d42770bdfe3992561f0d934565136
Frank Xu <frank.xu@northbright.com>
2024-09-24 20:57:03 +0800

    Add Progress type
    Progress can starts a new goroutine to call the callback to notify
    caller the progress copied.

---
+++ progress/progress.go
@@ -0,0 +1,106 @@
+package progress
+
+import (
+	"context"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/northbright/iocopy"
+)
+
+var (
+	DefaultInterval = time.Millisecond * 500
+)
+
+// Percent returns the percentage.
+// total: total number of the bytes to copy.
+// prev: the number of the bytes copied previously.
+// current: the number of bytes written currently.
+func Percent(total, prev, current int64) float32 {
+	if total == 0 {
+		return 0
+	}
+
+	return float32(float64(prev+current) / (float64(total) / float64(100)))
+}
+
+type Progress struct {
+	total    int64
+	prev     int64
+	current  int64
+	old      int64
+	lock     sync.RWMutex
+	fn       iocopy.OnWrittenFunc
+	interval time.Duration
+}
+
+type Option func(p *Progress)
+
+func OnWritten(fn iocopy.OnWrittenFunc) Option {
+	return func(p *Progress) {
+		p.fn = fn
+	}
+}
+
+func Interval(d time.Duration) Option {
+	return func(p *Progress) {
+		p.interval = d
+	}
+}
+
+func New(total, prev int64, options ...Option) *Progress {
+	p := &Progress{
+		total: total,
+		prev:  prev,
+	}
+
+	for _, option := range options {
+		option(p)
+	}
+
+	if p.interval <= 0 {
+		p.interval = DefaultInterval
+	}
+
+	return p
+}
+
+func (p *Progress) Write(b []byte) (n int, err error) {
+	n = len(b)
+	p.lock.Lock()
+	p.current += int64(n)
+	p.lock.Unlock()
+	return n, nil
+}
+
+func (p *Progress) callback() {
+	if p.fn != nil {
+		p.lock.RLock()
+		if p.current != p.old {
+			p.fn(p.total, p.prev, p.current, Percent(p.total, p.prev, p.current))
+			p.old = p.current
+		}
+		p.lock.RUnlock()
+	}
+}
+
+func (p *Progress) Start(ctx context.Context, chExit <-chan struct{}) {
+	ch := time.Tick(p.interval)
+
+	go func() {
+		for {
+			select {
+			case <-chExit:
+				log.Printf("on exit")
+				p.callback()
+				return
+			case <-ctx.Done():
+				log.Printf("on ctx.Done()")
+				return
+			case <-ch:
+				p.callback()
+			}
+		}
+	}()
+}
---
+++ progress/progress_test.go
@@ -0,0 +1,89 @@
+package progress_test
+
+import (
+	"context"
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/northbright/iocopy"
+	"github.com/northbright/iocopy/progress"
+)
+
+func ExampleNew() {
+	// This example uses iocopy.Copy to read stream from a remote file,
+	// and compute its SHA-256 checksum.
+	// It uses a timeout context to emulate user cancelation to stop the calculation.
+	// Then it calls iocopy.Copy again to resume the calculation.
+
+	// SHA-256: dd9e772686ed908bcff94b6144322d4e2473a7dcd7c696b7e8b6d12f23c887fd
+	url := "https://golang.google.cn/dl/go1.23.1.darwin-amd64.pkg"
+
+	// Get response body(io.Reader) of the remote file.
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Printf("http.Get() error: %v, url: %v", err, url)
+	}
+	defer resp.Body.Close()
+
+	// Get size of the remote file.
+	size := int64(0)
+	str := resp.Header.Get("Content-Length")
+	if str != "" {
+		size, _ = strconv.ParseInt(str, 10, 64)
+	}
+
+	// Create a hash.Hash(io.Writer) for SHA-256.
+	// hash.Hash is an io.Writer.
+	hash := sha256.New()
+
+	// Create a Progress which implements io.Writer interface.
+	p := progress.New(
+		// Total size.
+		size,
+		// Number of bytes copied previously.
+		0,
+		// OnWrittenFunc callback
+		progress.OnWritten(func(total, prev, current int64, percent float32) {
+			log.Printf("%v / %v(%.2f%%) bytes read and computed", current, total, percent)
+		}),
+	)
+
+	// Create a multiple writer and duplicate the writes to p.
+	mw := io.MultiWriter(hash, p)
+
+	ctx := context.Background()
+
+	// Create a channel and sent an empty struct to it when the function exits.
+	chExit := make(chan struct{}, 1)
+	defer func() {
+		chExit <- struct{}{}
+	}()
+
+	// Starts a new goroutine and a ticker to call the callback(OnWrittenFunc).
+	// The goroutine exits when an empty struct is send to chExit or ctx.Done().
+	p.Start(ctx, chExit)
+
+	// Call iocopy.Copy to read stream of the remote file and compute SHA-256 checksum.
+	// It blocks the main goroutine.
+	n, err := iocopy.Copy(ctx, mw, resp.Body)
+	if err != nil {
+		if err != context.Canceled && err != context.DeadlineExceeded {
+			log.Printf("iocopy.Copy() error: %v", err)
+			return
+		}
+		log.Printf("iocopy.Copy() stopped, cause: %v", ctx.Err())
+	}
+	log.Printf("iocopy.Copy() OK. %v bytes copied", n)
+
+	// Get checksum.
+	checksum := hash.Sum(nil)
+	fmt.Printf("SHA-256:\n%x\n", checksum)
+
+	// Output:
+	// SHA-256:
+	// dd9e772686ed908bcff94b6144322d4e2473a7dcd7c696b7e8b6d12f23c887fd
+}
