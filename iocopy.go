c49c194c720252a21107739a7504a5564f17ee3d
Frank Xu <frank.xu@northbright.com>
2025-01-04 11:55:01 +0800

    Make code shorter by using writeFunc / readFunc var

--- iocopy.go
+++ iocopy.go
@@ -54,63 +54,6 @@ func computePercent(total, prev, current int64) float32 {
 	return float32(float64(prev+current) / (float64(total) / float64(100)))
 }
 
-// CopyWithProgress wraps [io.Copy]. It accepts [context.Context] to make IO copy cancalable.
-// It also accepts callback function on bytes written to report progress.
-// total: total number of bytes to copy.
-// prev: number of bytes copied previously.
-// It can be used to resume the IO copy.
-// 1. Set prev to 0 when call CopyWithProgress for the first time.
-// 2. User stops the IO copy and CopyWithProgress returns the number of bytes written and error.
-// 3. Check if err == context.Canceled || err == context.DeadlineExceeded.
-// 4. Set prev to the "written" return value of previous CopyWithProgress when make next call to resume the IO copy.
-// fn: callback on bytes written.
-func CopyWithProgress(
-	ctx context.Context,
-	dst io.Writer,
-	src io.Reader,
-	total int64,
-	prev int64,
-	fn OnWrittenFunc) (written int64, err error) {
-
-	var (
-		current    int64
-		percent    float32
-		oldPercent float32
-	)
-
-	return io.Copy(
-		writeFunc(func(p []byte) (n int, err error) {
-			select {
-			case <-ctx.Done():
-				return 0, ctx.Err()
-			default:
-				n, err = dst.Write(p)
-				if err != nil {
-					return n, err
-				}
-
-				if fn != nil {
-					current += int64(n)
-					percent = computePercent(total, prev, current)
-					if percent != oldPercent {
-						fn(total, prev, current, percent)
-						oldPercent = percent
-					}
-				}
-				return n, nil
-			}
-		}),
-		readFunc(func(p []byte) (n int, err error) {
-			select {
-			case <-ctx.Done():
-				return 0, ctx.Err()
-			default:
-				return src.Read(p)
-			}
-		}),
-	)
-}
-
 // CopyBufferWithProgress wraps [io.CopyBuffer]. It accepts [context.Context] to make IO copy cancalable.
 // It also accepts callback function on bytes written to report progress.
 // total: total number of bytes to copy.
@@ -136,67 +79,69 @@ func CopyBufferWithProgress(
 		oldPercent float32
 	)
 
-	return io.CopyBuffer(
-		writeFunc(func(p []byte) (n int, err error) {
-			select {
-			case <-ctx.Done():
-				return 0, ctx.Err()
-			default:
-				n, err = dst.Write(p)
-				if err != nil {
-					return n, err
-				}
+	writeFn := writeFunc(func(p []byte) (n int, err error) {
+		select {
+		case <-ctx.Done():
+			return 0, ctx.Err()
+		default:
+			n, err = dst.Write(p)
+			if err != nil {
+				return n, err
+			}
 
-				if fn != nil {
-					current += int64(n)
-					percent = computePercent(total, prev, current)
-					if percent != oldPercent {
-						fn(total, prev, current, percent)
-						oldPercent = percent
-					}
+			if fn != nil {
+				current += int64(n)
+				percent = computePercent(total, prev, current)
+				if percent != oldPercent {
+					fn(total, prev, current, percent)
+					oldPercent = percent
 				}
-				return n, nil
 			}
-		}),
-		readFunc(func(p []byte) (n int, err error) {
-			select {
-			case <-ctx.Done():
-				return 0, ctx.Err()
-			default:
-				return src.Read(p)
-			}
-		}),
-		buf,
-	)
+			return n, nil
+		}
+	})
+
+	readFn := readFunc(func(p []byte) (n int, err error) {
+		select {
+		case <-ctx.Done():
+			return 0, ctx.Err()
+		default:
+			return src.Read(p)
+		}
+	})
+
+	if fn != nil {
+		if buf != nil && len(buf) > 0 {
+			return io.CopyBuffer(writeFn, readFn, buf)
+		} else {
+			return io.Copy(writeFn, readFn)
+		}
+	} else {
+		if buf != nil && len(buf) > 0 {
+			return io.CopyBuffer(dst, readFn, buf)
+		} else {
+			return io.Copy(dst, readFn)
+		}
+	}
 }
 
 // Copy wraps [io.Copy]. It accepts [context.Context] to make IO copy cancalable.
 func Copy(ctx context.Context, dst io.Writer, src io.Reader) (written int64, err error) {
-	return io.Copy(
-		dst,
-		readFunc(func(p []byte) (n int, err error) {
-			select {
-			case <-ctx.Done():
-				return 0, ctx.Err()
-			default:
-				return src.Read(p)
-			}
-		}),
-	)
+	return CopyBufferWithProgress(ctx, dst, src, nil, 0, 0, nil)
 }
 
 // CopyBuffer wraps [io.CopyBuffer]. It accepts [context.Context] to make IO copy cancalable.
 func CopyBuffer(ctx context.Context, dst io.Writer, src io.Reader, buf []byte) (written int64, err error) {
-	return io.CopyBuffer(
-		dst,
-		readFunc(func(p []byte) (n int, err error) {
-			select {
-			case <-ctx.Done():
-				return 0, ctx.Err()
-			default:
-				return src.Read(p)
-			}
-		}),
-		buf,
-	)
+	return CopyBufferWithProgress(ctx, dst, src, buf, 0, 0, nil)
+}
+
+// CopyWithProgress is the non-buffered version of [CopyBufferWithProgress].
+func CopyWithProgress(
+	ctx context.Context,
+	dst io.Writer,
+	src io.Reader,
+	total int64,
+	prev int64,
+	fn OnWrittenFunc) (written int64, err error) {
+	return CopyBufferWithProgress(ctx, dst, src, nil, total, prev, fn)
 }
